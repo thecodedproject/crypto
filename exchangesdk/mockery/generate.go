57a56c193bd32e24cfd243f85dc749548ac71b56
Jonny Cooper <jcooper@luno.com>
2021-03-21 18:04:02 +0000

    exchangesdk/mockery: Add mockery client

---
+++ exchangesdk/mockery/generate.go
@@ -0,0 +1,7 @@
+package mockery
+
+import "github.com/thecodedproject/crypto/exchangesdk"
+
+//go:generate mockery -dir=.. -outpkg=mockery -output=. -case=snake -name=Client
+
+var _ exchangesdk.Client = (*Client)(nil)
