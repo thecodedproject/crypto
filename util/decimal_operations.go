3ef4b222a909061c5c864176528e35c7a3f5a8f2
Jonny Cooper <jcooper@luno.com>
2021-03-24 18:01:23 +0000

    util: Deprecate round down

--- util/decimal_operations.go
+++ util/decimal_operations.go
@@ -4,17 +4,10 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+// DEPRECATED: User decimal.Decimal.Truncate instead
 // RoundDown will round a decimal to a precision, always rounding towards zero
 // E.g. rounding 1.36 to 1 d.p. gives 1.3
 func RoundDown(d decimal.Decimal, precision int32) decimal.Decimal {
 
-	if d.Sign() == 0 {
-		return d
-	} else if d.Sign() == 1 {
-		d = d.Sub(decimal.New(5, -precision-1))
-	} else {
-		d = d.Add(decimal.New(5, -precision-1))
-	}
-	d = d.Round(precision)
-	return d
+	return d.Truncate(precision)
 }
