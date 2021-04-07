73a6e7570f9c179c54d9778e292c8a9fb5437dca
Jonny Cooper <jcooper@luno.com>
2021-04-07 15:25:18 +0100

    tools/api_poker: Add get cancel and custom methods

--- tools/api_poker/main.go
+++ tools/api_poker/main.go
@@ -10,8 +10,10 @@ import (
 	"log"
 	"flag"
 	"fmt"
+	"encoding/json"
 
 	"github.com/thecodedproject/crypto"
+	"github.com/shopspring/decimal"
 	"github.com/thecodedproject/crypto/exchangesdk"
 	"github.com/thecodedproject/crypto/exchangesdk/factory"
 	"github.com/thecodedproject/crypto/io"
@@ -21,7 +23,8 @@ var (
 	authName = flag.String("api_auth", "", "API auth name to use")
 	authPath = flag.String("auth_path", "api_auth.json", "Auth file path")
 	pairName = flag.String("pair", "btcusdt", "Exchange pair to use")
-	runGetCommand = flag.Bool("get", false, "Run get command")
+	runGetCommand = flag.Bool("get", false, "Run get order command")
+	runCancelCommand = flag.Bool("cancel", false, "Run cancel order command")
 	runCustomCommand = flag.Bool("custom", false, "Run custom command")
 )
 
@@ -30,6 +33,7 @@ type Command int
 const (
 	CommandUnknown Command = iota
 	CommandGet
+	CommandCancel
 	CommandCustom
 	CommandSentinal
 )
@@ -39,7 +43,103 @@ func getCommand(
 	exchangeClient exchangesdk.Client,
 ) error {
 
+	if flag.NArg() != 1 {
+		return fmt.Errorf("Need order ID for get command;\nUSAGE api_poker --get <order_id>")
+	}
+
+	orderStatus, err := exchangeClient.GetOrderStatus(
+		ctx,
+		flag.Arg(0),
+	)
+	if err != nil {
+		return err
+	}
+
+	str, err := json.Marshal(orderStatus)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(str))
+
 	return nil
+}
+
+func cancelCommand(
+	ctx context.Context,
+	exchangeClient exchangesdk.Client,
+) error {
+
+	if flag.NArg() != 1 {
+		return fmt.Errorf("Need order ID for cancel command;\nUSAGE api_poker --cancel <order_id>")
+	}
+
+	orderID := flag.Arg(0)
+
+	err := exchangeClient.CancelOrder(
+		ctx,
+		orderID,
+	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Cancelled order", orderID)
+
+	return nil
+}
+
+func postLimitOrder(
+	ctx context.Context,
+	exchangeClient exchangesdk.Client,
+) error {
+
+	orderID, err := exchangeClient.PostLimitOrder(
+		ctx,
+		exchangesdk.Order{
+			Type: exchangesdk.OrderTypeBid,
+			Price: decimal.NewFromFloat(50010),
+			Volume: decimal.NewFromFloat(0.001),
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(orderID)
+
+	return nil
+}
+
+func postStopLimitOrder(
+	ctx context.Context,
+	exchangeClient exchangesdk.Client,
+) error {
+
+	orderID, err := exchangeClient.PostStopLimitOrder(
+		ctx,
+		exchangesdk.StopLimitOrder{
+			Side: exchangesdk.OrderBookSideAsk,
+			StopPrice: decimal.NewFromFloat(56855),
+			LimitPrice: decimal.NewFromFloat(56850),
+			Volume: decimal.NewFromFloat(0.0002),
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(orderID)
+
+	return nil
+}
+
+func runCustom(
+	ctx context.Context,
+	exchangeClient exchangesdk.Client,
+) error {
+	fmt.Println("run custom...")
+	return postStopLimitOrder(ctx, exchangeClient)
 }
 
 func runCommand(
@@ -50,8 +150,9 @@ func runCommand(
 
 	switch command {
 	case CommandGet:
-		log.Println("Run get...")
-		return nil
+		return getCommand(ctx, exchangeClient)
+	case CommandCancel:
+		return cancelCommand(ctx, exchangeClient)
 	case CommandCustom:
 		//reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter `custom` to confirm command run: ")
@@ -61,8 +162,7 @@ func runCommand(
 		if confirm != "custom" {
 			return fmt.Errorf("wrong confirm code - refusing to run custom command")
 		}
-		log.Println("Run custom...")
-		return nil
+		return runCustom(ctx, exchangeClient)
 	default:
 		return fmt.Errorf("Unknown command")
 	}
@@ -71,7 +171,7 @@ func runCommand(
 func validateCmdArgs() {
 
 	if *authName == "" {
-		log.Fatal("auth_name is required")
+		log.Fatal("api_auth is required")
 	}
 }
 
@@ -79,6 +179,9 @@ func resolveCommand() Command {
 
 	if *runGetCommand {
 		return CommandGet
+	}
+	if *runCancelCommand {
+		return CommandCancel
 	}
 	if *runCustomCommand {
 		return CommandCustom
